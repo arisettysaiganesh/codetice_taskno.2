b2ff488e7c83ba6b8d549e439618aa92a6607c29
Sai Ganesh Arisetty <86103704+arisettysaiganesh@users.noreply.github.com>
2023-10-08 00:02:07 +0530

    playr.py

--- playr.py
+++ playr.py
@@ -1,40 +1,55 @@
-# PiePlayer (c) ArtGames101 2018
-import pygame, sys, os, subprocess, time
+import pygame
+import os
+import time
 import random
 import config
 
-IS_WINDOWS = os.name == "nt"
-IS_MAC = sys.platform == "darwin"
-
 def clear_screen():
-    if IS_WINDOWS:
-        os.system("cls")
-    else:
-        os.system("clear")
+    os.system("cls" if os.name == "nt" else "clear")
 
-def user_choice():
-    return input("\n>>> ").lower().strip()
+def list_songs():
+    song_files = os.listdir("songs")
+    return [song for song in song_files if song.endswith((".mp3", ".ogg"))]
+
+def play_song(song):
+    pygame.mixer.init()
+    try:
+        pygame.mixer.music.load(os.path.join("songs", song))
+        pygame.mixer.music.play()
+    except pygame.error:
+        input("Unable to play/load the selected song. Press Enter to continue...")
 
 def main():
     clear_screen()
-    print("Choose a song!\n")
-    subprocess.call(("dir", "songs"))
-    choice = user_choice()
-    clear_screen()
-    print("Playing Song... [{}]\n"
-          "\n"
-          "CTRL + C Exit".format(time.ctime()))
-    pygame.mixer.init()
-    try:
-        pygame.mixer.music.load("songs/{}".format(choice))
-        pygame.mixer.music.play()
-    except:
+    print("Welcome to SGPlayer!\n")
+    
+    songs = list_songs()
+    
+    if not songs:
+        input("No songs found in the 'songs' directory. Press Enter to exit...")
+        return
+    
+    while True:
         clear_screen()
-        input("Unable to play/load a song!")
-        main()
-    while pygame.mixer.music.get_busy() == True:
-        continue
+        print("Choose a song:\n")
+        for i, song in enumerate(songs, start=1):
+            print(f"{i}. {song}")
+        print("\n0. Exit")
+        
+        choice = input("\nEnter the number of the song you want to play (0 to exit): ").strip()
+        
+        if choice == "0":
+            break
+        
+        try:
+            choice = int(choice)
+            if 1 <= choice <= len(songs):
+                selected_song = songs[choice - 1]
+                clear_screen()
+                print(f"Playing '{selected_song}'... [ {time.ctime()} ]\n")
+                play_song(selected_song)
+        except (ValueError, IndexError):
+            input("Invalid input. Press Enter to continue...")
+
+if __name__ == "__main__":
     main()
-
-
-main()
