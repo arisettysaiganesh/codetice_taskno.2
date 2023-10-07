1499c35d8db44492f994e3cdd089731a1f7ffcc5
Sai Ganesh Arisetty <86103704+arisettysaiganesh@users.noreply.github.com>
2023-10-08 00:10:44 +0530

    run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-# PiePlayer (c) ArtGames101 2018
-import pygame, sys, os, subprocess, webbrowser
+import os
+import subprocess
+import webbrowser
 
 IS_WINDOWS = os.name == "nt"
-IS_MAC = sys.platform == "darwin"
 
 def clear_screen():
     if IS_WINDOWS:
@@ -10,52 +10,58 @@ def clear_screen():
     else:
         os.system("clear")
 
-def user_choice():
-    return input("\n>>> ").lower().strip()
+def user_choice(options):
+    while True:
+        choice = input("\n>>> ").lower().strip()
+        if choice in options:
+            return choice
+        else:
+            print("Invalid choice. Please try again.")
 
-def main():
+def play_songs():
     clear_screen()
-    print("PiePlayer\n"
-          "================")
+    print("SGPlayer Audio\n"
+          "===================")
     print("\n"
-          "l. Play Songs\n"
-          "\n"
-          "w. Find Songs on the web")
-    choice = user_choice()
-    if choice == "l":
+          "1. Play Playlist (Python)\n"
+          "2. Play A Song (Python 3)\n"
+          "i. Info\n"
+          "0. Back")
+    choice = user_choice(["1", "2", "i", "0"])
+    
+    if choice == "1":
+        subprocess.call(("python", "play.py"))
+    elif choice == "2":
+        subprocess.call((sys.executable, "playr.py"))
+    elif choice == "i":
         clear_screen()
         print("PiePlayer Audio\n"
               "===================")
         print("\n"
-              "1. Play Playlist (Python)\n"
+              "Playlist:\n"
+              "The Playlist is looped and shuffled!\n"
               "\n"
-              "2. Play A Song (Python 3)\n"
-              "\n"
-              "i. Info\n"
-              "0. Back")
-        choice = user_choice()
-        if choice == "1":
-            subprocess.call(("python", "play.py"))
-        if choice == "2":
-            subprocess.call((sys.executable, "playr.py"))
-        if choice == "i":
-            clear_screen()
-            print("PiePlayer Audio\n"
-                  "===================")
-            print("\n"
-                  "Playlist:\n"
-                  "The Playlist is looped and shuffled!\n"
-                  "\n"
-                  "Play Song:\n"
-                  "Choose a song to play (is not looped)")
-            input("\n"
-                  "Back")
-            main()
-    if choice == "w":
+              "Play Song:\n"
+              "Choose a song to play (is not looped)")
+        input("\n"
+              "Back")
+
+def main():
+    clear_screen()
+    print("SGPlayer\n"
+          "================")
+    print("\n"
+          "l. Play Songs\n"
+          "w. Find Songs on the web")
+    choice = user_choice(["l", "w"])
+
+    if choice == "l":
+        play_songs()
+    elif choice == "w":
         webbrowser.open("https://ganna.com")
-        print("Opening Please wait...")
-        time.sleep(5)
+        print("Opening, please wait...")
+        input("Press Enter to continue.")
         main()
 
-main()
-
+if __name__ == "__main__":
+    main()
